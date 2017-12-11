7a4f1f79b0ad287fc1154d887b31c1543e0bd335
roy ledford <bluesixty@gmail.com>
2017-12-11 14:46:03 -0500

    Add drag and drop component

---
+++ src/components/DragAndDrop/ReactDnD.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import StateView from './StateView'
+import ReactDnDList from './ReactDnDList'
+import './DnD.css'
+
+export default class ReactDnD extends Component {
+  static propTypes = {
+    houses: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        name: PropTypes.string.isRequired,
+        sortIndex: PropTypes.number.isRequired,
+      })
+    ).isRequired,
+  }
+  static defaultProps = {
+    data: {},
+  }
+
+  constructor(props) {
+    super(props)
+
+    this.moveCard = this.moveCard.bind(this)
+  }
+
+  moveCard(dragIndex, hoverIndex) {
+    const cards = Object.assign([], this.props.houses)
+    const dragCard = cards[dragIndex]
+
+    // remove the dragged card
+    cards.splice(dragIndex, 1)
+
+    // add it back in at the hover position
+    cards.splice(hoverIndex, 0, dragCard)
+
+    // update the sort index
+    cards.forEach((card, i) => (card.sortIndex = i))
+    this.props.handleDataChanged(cards)
+  }
+
+  render() {
+    const { houses } = this.props
+    return (
+      <div>
+        <h3 className="dnd-title">Drag and Drop using react-dnd</h3>
+        <div className="dnd">
+          <div className="dnd-column">
+            <ReactDnDList data={houses} moveCard={this.moveCard} />
+          </div>
+          <StateView state={houses} />
+        </div>
+      </div>
+    )
+  }
+}
---
+++ src/components/DragAndDrop/ReactDnDList.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { DragDropContext } from 'react-dnd'
+import HTML5Backend from 'react-dnd-html5-backend'
+
+import ReactDnDListItem from './ReactDnDListItem'
+import './List.css'
+
+class ListDnD extends Component {
+  static propTypes = {
+    data: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        name: PropTypes.string.isRequired,
+        sortIndex: PropTypes.number.isRequired,
+      })
+    ).isRequired,
+  }
+  static defaultProps = {
+    data: {},
+  }
+
+  render() {
+    const { data } = this.props
+
+    const listItems = data.map((card, i) => {
+      return <ReactDnDListItem key={card.id} index={i} text={card.name} moveCard={this.props.moveCard} />
+    })
+
+    return (
+      <ul className="list" onDragOver={this.dragOver}>
+        {listItems}
+      </ul>
+    )
+  }
+}
+
+export default DragDropContext(HTML5Backend)(ListDnD)
